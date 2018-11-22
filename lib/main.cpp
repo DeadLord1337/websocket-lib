db8e66a803e630b119cb45abb8f49d0b7c05e365
B0RY5 <b0ry5.dev@gmail.com>
2018-11-22 19:11:16 +0000

    Optimisation

--- lib/main.cpp
+++ lib/main.cpp
@@ -1,1 +1,15 @@
 // Im 15, why the fuck are you browsing through me, im calling the police
+
+//     _
+//   _| |
+// _| | |
+//| | | |
+//| | | | __
+//| | | |/  \
+//|       /\ \
+//|      /  \/
+//|      \  /\   lmao goteem
+//|       \/ /
+// \        /
+//  |     /
+//  |    |
